08b30fd9ed2da8f067573570ff8b7bcd4641e72e
SuMyatThandar99 <sumyatthandar.smthandar@gmail.com>
2026-02-09 00:52:36 +0900

    Create EC2 instance with Apache web server setup
    This script creates an EC2 instance with Apache installed and a welcome page.

---
+++ ec2-boto3-2.py
@@ -0,0 +1,43 @@
+import boto3
+
+ec2 = boto3.resource('ec2')
+
+instances = ec2.create_instances(
+    ImageId='ami-0f65fc8c24ec8d2a1',  # Ubuntu 22.04 (ap-northeast-1)
+    MinCount=1,
+    MaxCount=1,
+    InstanceType='t3.micro',
+    KeyName='my-keypair-name',
+    SecurityGroupIds=['sg-xxxxxxxx'],
+    BlockDeviceMappings=[
+        {
+            'DeviceName': '/dev/sda1',
+            'Ebs': {
+                'VolumeSize': 20,
+                'VolumeType': 'gp3',
+                'DeleteOnTermination': True
+            }
+        }
+    ],
+    UserData='''#!/bin/bash
+apt update -y
+apt install apache2 -y
+systemctl start apache2
+systemctl enable apache2
+
+echo "<html><body><h1>Welcome to Apache Web Server</h1></body></html>" > /var/www/html/index.html
+''',
+    TagSpecifications=[
+        {
+            'ResourceType': 'instance',
+            'Tags': [
+                {'Key': 'Name', 'Value': 'Pythontest'},
+                {'Key': 'Department', 'Value': 'Technical'},
+                {'Key': 'Environment', 'Value': 'Test'}
+            ]
+        }
+    ]
+)
+
+for instance in instances:
+    print(f"Instance {instance.id} is launching")
